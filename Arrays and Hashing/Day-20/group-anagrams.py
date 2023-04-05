b1ae4b99fba39f9b80fd51b8d0cf63c58ea96501
Krishna Gupta <krishna_gupta@epam.com>
2023-04-06 00:06:08 +0545

    day-21

---
+++ Arrays and Hashing/Day-20/group-anagrams.py
@@ -0,0 +1,15 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+
+        for s in strs:
+            count = [0]*26
+            for c in s:
+                count[ord(c)-ord("a")] +=1
+            result[tuple(count)].append(s)
+        return result.values()
+
+# n*m n->length of char, m -> length of list
